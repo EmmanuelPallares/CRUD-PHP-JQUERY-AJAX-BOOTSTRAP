eee42fb5ed9abc96435a90d0b1fea3e15242fb7d
Emmanuel Pallares <emmanuelpallares50@gmail.com>
2021-12-06 00:25:03 -0600

    insercion a la base de datos simple

--- index.php
+++ index.php
@@ -11,3 +11,14 @@ echo "<h1>Conectado</h1>";
 }catch(Exception $ex){
 echo $ex->getMessage();
 }
+
+if (isset($_GET['accion'])=="insert") {
+$nombre = "LOGITECH";
+
+$precio= 450;
+$sentenciasql=$conexion->prepare("INSERT INTO teclados (nombre, precio) VALUES (:nombre, :precio)");
+$sentenciasql->bindParam(':nombre', $nombre);
+$sentenciasql->bindParam(':precio', $precio);
+$sentenciasql->execute();
+
+}
